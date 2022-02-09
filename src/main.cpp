70c51947dc3b7b0ef4a16254e40e2e10d12ed76e
Christian Le <crezle1999@gmail.com>
2022-02-10 00:21:57 +0100

    write new code from scratch

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,53 @@
-#include "Arduino.h"
+#include <SPI.h>        
+#include <NativeEthernet.h>
+#include <NativeEthernetUdp.h>
 
-void setup()
-{
+
+
+// Enter a MAC address and IP address for your controller below.
+
+// The IP address will be dependent on your local network:
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+IPAddress ip(192, 168, 1, 177);
+
+unsigned int localPort = 8888;      // local port to listen on
+
+char ReplyBuffer[] = "messageSent";
+
+// An EthernetUDP instance to let us send and receive packets over UDP
+
+EthernetUDP Udp;
+
+void setup() {
+
+  // start the Ethernet and UDP:
+
+  Ethernet.begin(mac,ip);
+
+  Udp.begin(localPort);
+
+  pinMode(LED_BUILTIN, OUTPUT);
 
 }
 
-void loop()
-{
 
+
+void loop() {
+
+  Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+
+    Udp.write(ReplyBuffer);
+
+    int status = Udp.endPacket();
+    
+    if (status == 1) {
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+    else {
+      digitalToggle(LED_BUILTIN);
+    }
+
+    delay(100);
 }
-
-
-/*
-  Processing sketch to run with this example
- =====================================================
-
- // Processing UDP example to send and receive string data from Arduino
- // press any key to send the "Hello Arduino" message
-
-
- import hypermedia.net.*;
-
- UDP udp;  // define the UDP object
-
-
- void setup() {
- udp = new UDP( this, 6000 );  // create a new datagram connection on port 6000
- //udp.log( true ); 		// <-- printout the connection activity
- udp.listen( true );           // and wait for incoming message
- }
-
- void draw()
- {
- }
-
- void keyPressed() {
- String ip       = "192.168.1.177";	// the remote IP address
- int port        = 8888;		// the destination port
-
- udp.send("Hello World", ip, port );   // the message to send
-
- }
-
- void receive( byte[] data ) { 			// <-- default handler
- //void receive( byte[] data, String ip, int port ) {	// <-- extended handler
-
- for(int i=0; i < data.length; i++)
- print(char(data[i]));
- println();
- }
- */
