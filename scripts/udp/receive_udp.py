4879e2b023acd5c19002e1c80a0919af1d6619cf
Christian Le <crezle1999@gmail.com>
2022-02-20 16:26:51 +0100

    add own folder for scripts

--- scripts/udp/receive_udp.py
+++ scripts/udp/receive_udp.py
@@ -4,13 +4,6 @@ import os
 UDP_IP = "10.0.0.2"
 UDP_PORT = 8888
 
-REMOTE_IP = "129.241.187.177"
-
-LOCALHOST_IP = "127.0.0.1"
-
-''' while not("Received = 4" in os.popen(f"ping {REMOTE_IP}").read()):
-    pass '''
-
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP,UDP_PORT))
