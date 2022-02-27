f26e46ea4cd525784d4e9e06477103ea8e56311b
Christian Le <crezle1999@gmail.com>
2022-02-27 15:05:16 +0100

    include headerConfig.h to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <ethernetModule.h>
+#include <headerConfig.h>
 #include <Arduino.h>
 
 int main() {
   serialAndLEDSetup();
   ethernetSetup();
-  
+
   while(1) {
     ethernetCheck();
   }
