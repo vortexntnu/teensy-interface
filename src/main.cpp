e2c9875ca5f3a39716287a93f85bb4302b362bf7
Christian Le <crezle1999@gmail.com>
2022-03-13 18:35:20 +0100

    implement GPIO interrupt, PROBLEM: registers not changing

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,10 @@
 #include <ethernetModule.h>
 #include <Arduino.h>
-
-char defaultMessage[] = "No package received";
-// This can be any wanted character array / string
+#include <gpioInterrupt.h>
 
 int main() {
-  eth::setup();
-
+  setupGPIOInterrupt();
   while(1) {
-    char* rec_message = eth::read();
-    if(rec_message != nullptr) {
-        eth::write(rec_message);
-    }
-    else {
-        eth::write(defaultMessage);
-    }
     delay(1000);
   }
 }
