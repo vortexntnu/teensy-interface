87493398218a3d8184d9683df9f90801b5a95c1c
Christian Le <crezle1999@gmail.com>
2022-03-20 15:03:43 +0100

    implement pin 28, 30, 31 trigger on active high input

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,13 @@
 #include <gpioInterrupt.h>
 
 int main() {
+  Serial.begin(9600);
+  while (!Serial) {}
   setupGPIOInterrupt();
   while(1) {
+
+    Serial.printf("TOUCHED PINS: %X\r\n", GPIO8_DR);
     delay(1000);
+    GPIO8_DR &= ~0xC40000;
   }
 }
